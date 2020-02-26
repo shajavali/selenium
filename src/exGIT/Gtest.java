782560780c820962c441e4cf166d5435093e5f75
shajavali <shajavalishaik26@gmail.com>
2020-02-26 16:57:23 +0530

    add new change shaja

--- src/exGIT/Gtest.java
+++ src/exGIT/Gtest.java
@@ -5,6 +5,7 @@ public class Gtest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello dimpal");
+		System.out.println("hello shaja");
 
 	}
 
